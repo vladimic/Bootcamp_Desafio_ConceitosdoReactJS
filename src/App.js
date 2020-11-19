d0a47b4cb38a7d6d90620f4bc9b040e81f988344
Vladimir Michels <vladimir.michels@gmail.com>
2020-11-19 16:09:33 -0300

    versao final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: 'www.eu.com.br',
+      owner: 'Vladimir Michels'
+    });
+
+    const repository = response.data;
+
+    setRepositories([ ... repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
+
+    await api.delete(`/repositories/${id}`); // aqui elimina a api do back-end
+
+    setRepositories(repositories.filter( // mas o objeto continua em tela.
+      repository => repository.id !== id // então preciso fazer um filtro, para 
+    ))                                   // nao ver o objetivo eliminado
+    
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list"> 
+          {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title} 
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)}          
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+
     </div>
   );
 }
